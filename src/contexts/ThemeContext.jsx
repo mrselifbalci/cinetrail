e1f646cc4684f5e8d9a66c7362124003e5960b31
Elif Balci <mrselifbalci@gmail.com>
2023-06-16 07:25:11 +0100

    credentials changed

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,25 @@
+import React,{useState,createContext,useEffect} from 'react'
+export const ThemeContext = createContext()
+
+
+
+export default function ThemeContextProvider(props) {
+    const [darkMode,setDarkMode]=useState(true)
+
+
+    useEffect(() => {
+      const theme = localStorage.getItem('darkMode')
+      if (theme) {
+        setDarkMode(JSON.parse(theme))
+      }
+    }, [])
+    
+ 
+
+ 
+  return (
+    <ThemeContext.Provider value={{darkMode,setDarkMode}}>
+        {props.children}
+    </ThemeContext.Provider>
+  )
+} 
